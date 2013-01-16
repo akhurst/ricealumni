294b4c06532ee0f0a8a4e090a99269bc4cb3fcab
Allen Hurst <allenhurst@gmail.com>
2013-01-16 14:11:39 -0600

    updating home page

---
+++ orchard/src/RiceAlumni.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiceAlumni.Theme.Helpers;
+
+namespace RiceAlumni.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var events = PciEventCalendarHelper.GetEvents();
+		}
+	}
+}
