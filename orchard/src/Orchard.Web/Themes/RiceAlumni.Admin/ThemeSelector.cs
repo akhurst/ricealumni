dff5e95a7a0176a3d6bd81c5cbf29c81d94dc28d
Allen Hurst <allenhurst@gmail.com>
2013-01-25 00:01:30 -0600

    adding more of ryan's stuff

---
+++ orchard/src/Orchard.Web/Themes/RiceAlumni.Admin/ThemeSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using Orchard.Themes;
+using Orchard.UI.Admin;
+
+namespace RiceAlumni.Admin
+{
+//	public class OverrideTheAdmin : IThemeSelector
+//	{
+//		public ThemeSelectorResult GetTheme(RequestContext context)
+//		{
+//			if (AdminFilter.IsApplied(context))
+//			{
+//				return new ThemeSelectorResult { Priority = 110, ThemeName = "RiceAlumni.Admin" };
+//			}
+//
+//			return null;
+//		}
+//	}
+}
