b6e1f26adbc5336f8562730e885c09f660c22272
Allen Hurst <allenhurst@gmail.com>
2013-01-30 18:07:46 -0600

    fixing legacy content routing

---
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Core/Controllers/LegacyContentController.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace RiceAlumni.Core.Controllers
+{
+	public class LegacyContentController : Controller
+	{
+		public ActionResult RenderLegacyContent(string path)
+		{
+			if (path == "index.html")
+				return Redirect("~/");
+
+			var splitPath = path.Split('/');
+			var finalPath = path;
+
+			if(splitPath.Length > 0 && !splitPath[splitPath.Length-1].Contains("."))
+			{
+				finalPath = finalPath + "/index.html";
+			}
+			return Redirect("legacycontent/"+finalPath);
+
+//			var filePath = LegacyContentConstraint.FindPath(HttpContext, finalPath);
+//			var stream = new FileStream(filePath, FileMode.Open);
+//			return new FileStreamResult(stream, "text/html");
+		}
+	}
+}
