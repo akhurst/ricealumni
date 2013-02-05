d775f8d4dc4952f5e24b12e4f7546c810990fb76
Allen Hurst <allenhurst@gmail.com>
2013-02-05 16:46:11 -0600

    prod fixes

--- orchard/src/Orchard.Web/LegacyContent/ryanmagic.js
+++ orchard/src/Orchard.Web/LegacyContent/ryanmagic.js
@@ -1,8 +1,8 @@
 $("body").prepend("<div class='thenewhead'></div>");
 //$(".thenewhead").prepend("<div id='section-topbar'><div id='topbar-inner'><div class='container'><div class='sixteen columns'><div class='dropdown'><img src='headimages/rice_alumni_logo.png' class='headlogo' /><ul id='nav' class='nav'><li class='menu-item'><a href='#'>Events Calendar</a></li><li class='menu-item'><a href='#'>Alumni Groups</a></li><li class='menu-item'><a href='#'>Young Alumni</a></li><li class='menu-item'><a href='#'>Career Connection</a></li><li class='menu-item'><a href='#'>Volunteer</a></li><li class='menu-item'><a href='#'>Learning & Travel</a></li></div></div></div></div>");
 $('body table:first').wrap('<div class="newholder" />');
-$("body table:first").css("margin","0 auto 0 auto")
-$("body table:first").css("border-top","1px solid #eee")
+$("body table:first").css("margin", "0 auto 0 auto");
+$("body table:first").css("border-top", "1px solid #eee");
 $("body table tbody tr td table tbody tr:first").remove();
 $("body table:first").css("width","960px");
 $('body table tbody tr td table tr td table tbody tr td').has('form#srch').remove();
