0896461b61357057c361735f47cb0686eae72c6a
justinSelf <justin.self@improvingenterprises.com>
2013-02-11 15:56:12 -0600

    files missing from last commit

---
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Models/EventContactPart.cs
@@ -0,0 +1,29 @@
+﻿using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiceAlumni.Events.Models
+{
+    public class EventContactPart : ContentPart<EventContactRecord>
+    {
+        public string Name
+        {
+            get { return Record.Name; }
+            set { Record.Name = value; }
+        }
+
+        public long Phone
+        {
+            get { return Record.Phone; }
+            set { Record.Phone = value; }
+        }
+
+        public string Email
+        {
+            get { return Record.Email; }
+            set { Record.Email = value; }
+        }
+    }
+}
---
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Models/EventContactRecord.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.ContentManagement.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiceAlumni.Events.Models
+{
+    public class EventContactRecord : ContentPartRecord
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+    }
+}
