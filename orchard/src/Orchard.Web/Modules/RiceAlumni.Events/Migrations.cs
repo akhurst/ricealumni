b2bf48a21669364f8b08022ec07b02bfb8b6a2ca
justinSelf <justin.self@improvingenterprises.com>
2013-02-11 15:55:51 -0600

    fleshed out event objects

--- orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Migrations.cs
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Migrations.cs
@@ -8,75 +8,31 @@ using Orchard.Data.Migration;
 
 namespace RiceAlumni.Events
 {
-	public class Migrations : DataMigrationImpl
-	{
-//		public int Create()
-//		{
-//			SchemaBuilder.CreateTable("EventPartRecord", table => table
-//				.ContentPartRecord()
-//				.Column<string>("LinkText")
-//				.Column<string>("LinkTarget")
-//				.Column<DateTime>("StartDate")
-//				.Column<DateTime>("EndDate")
-//				.Column<int>("PciEventId")
-//			);
-//
-//			SchemaBuilder.CreateTable("LocationPartRecord", table => table
-//				.ContentPartRecord()
-//				.Column<string>("Name")
-//				.Column<string>("Building")
-//				.Column<string>("Room")
-//				.Column<string>("Address1")
-//				.Column<string>("Address2")
-//				.Column<string>("City")
-//				.Column<string>("State")
-//				.Column<string>("Zip")
-//				.Column<string>("MapUrl")
-//				.Column<double>("Latitude")
-//				.Column<double>("Longitude")
-//			);
-//
-//			ContentDefinitionManager.AlterPartDefinition("HomepageSecondaryImage", part => part
-//				.WithField("Image", field => field
-//					.OfType("ImageField")
-//					.WithSetting("ImageFieldSettings.Width", "691")
-//					.WithSetting("ImageFieldSettings.Height", "95")
-//					.WithSetting("ImageFieldSettings.ResizeAction", "Validate")
-//				)
-//			);
-//
-//			ContentDefinitionManager.AlterTypeDefinition("HomepageLink", builder => builder
-//				.Draftable()
-//				.WithPart("WidgetPart")
-//				.WithPart("CommonPart")
-//				.WithPart("BodyPart", partBuilder => partBuilder.WithSetting("BodyTypePartSettings.Flavor", "text"))
-//				.WithPart("HomepageSecondaryImage")
-//				.WithPart("HomepagePart")
-//				.WithPart("AdminMenuPart")
-//				.WithSetting("Stereotype", "Widget")
-//			);
-//
-//			ContentDefinitionManager.AlterPartDefinition("HomepagePrimaryImage", part => part
-//				.WithField("Image", field => field
-//					.OfType("ImageField")
-//					.WithSetting("ImageFieldSettings.Width", "1980")
-//					.WithSetting("ImageFieldSettings.Height", "650")
-//					.WithSetting("ImageFieldSettings.ResizeAction", "Validate")
-//				)
-//			);
-//
-//			ContentDefinitionManager.AlterTypeDefinition("HomepageSlide", builder => builder
-//				.Draftable()
-//				.WithPart("WidgetPart")
-//				.WithPart("CommonPart")
-//				.WithPart("BodyPart", partBuilder => partBuilder.WithSetting("BodyTypePartSettings.Flavor", "text"))
-//				.WithPart("HomepagePrimaryImage")
-//				.WithPart("HomepagePart")
-//				.WithPart("AdminMenuPart")
-//				.WithSetting("Stereotype", "Widget")
-//			);
-//
-//			return 1;
-//		}
-	}
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            SchemaBuilder.CreateTable("EventContactRecord", tableBuilder =>
+                tableBuilder.ContentPartRecord()
+                .Column<string>("Name")
+                .Column<string>("Email")
+                .Column<long>("Phone")
+                );
+
+            SchemaBuilder.CreateTable("EventPartRecord", tableBuilder =>
+                tableBuilder.Column<DateTime>("StartDate")
+                .ContentPartRecord()
+                .Column<DateTime>("EndDate")
+                .Column<string>("LinkTarget")
+                .Column<string>("LinkText")
+                .Column<int>("PciEvent")
+                .Column<string>("Title")
+                .Column<string>("Description")
+                .Column<bool>("RegistrationRequired")
+                .Column<int>("LocationPartRecord_Id")
+                );
+
+            return 1;
+        }
+    }
 }
--- orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Models/EventPart.cs
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Models/EventPart.cs
@@ -3,36 +3,60 @@ using Orchard.ContentManagement;
 
 namespace RiceAlumni.Events.Models
 {
-	public class EventPart : ContentPart<EventPartRecord>
-	{
-		public virtual DateTime StartDate
-		{
-			get { return Record.StartDate; }
-			set { Record.StartDate = value; }
-		}
+    public class EventPart : ContentPart<EventPartRecord>
+    {
+        public virtual DateTime StartDate
+        {
+            get { return Record.StartDate; }
+            set { Record.StartDate = value; }
+        }
 
-		public virtual DateTime EndDate
-		{
-			get { return Record.EndDate; }
-			set { Record.EndDate = value; }
-		}
+        public virtual DateTime EndDate
+        {
+            get { return Record.EndDate; }
+            set { Record.EndDate = value; }
+        }
 
-		public virtual string LinkTarget
-		{
-			get { return Record.LinkTarget; }
-			set { Record.LinkTarget = value; }
-		}
+        public virtual string LinkTarget
+        {
+            get { return Record.LinkTarget; }
+            set { Record.LinkTarget = value; }
+        }
 
-		public virtual string LinkText
-		{
-			get { return Record.LinkText; }
-			set { Record.LinkText = value; }
-		}
+        public virtual string LinkText
+        {
+            get { return Record.LinkText; }
+            set { Record.LinkText = value; }
+        }
 
-		public virtual int PciEventId
-		{
-			get { return Record.PciEventId; }
-			set { Record.PciEventId = value; }
-		}
-	}
+        public virtual int PciEventId
+        {
+            get { return Record.PciEventId; }
+            set { Record.PciEventId = value; }
+        }
+
+        public virtual string Title
+        {
+            get { return Record.Title; }
+            set { Record.Title = value; }
+        }
+
+        public virtual string Description
+        {
+            get { return Record.Description; }
+            set { Record.Description = value; }
+        }
+
+        public virtual LocationPart Location
+        {
+            get { return Record.Location; }
+            set { Record.Location = value; }
+        }
+
+        //public virtual EventContactPart Contact
+        //{
+        //    get { return Record.Contact; }
+        //    set { Record.Contact = value; }
+        //}         
+    }
 }
