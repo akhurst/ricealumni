85ecb5d278772c9848ab0744ce754d7b8c088062
justinSelf <justin.self@improvingenterprises.com>
2013-02-12 18:13:17 -0600

    can't get the events to persist... :(

---
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Drivers/EventPartDriver.cs
@@ -0,0 +1,33 @@
+﻿using Orchard.ContentManagement.Drivers;
+using RiceAlumni.Events.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiceAlumni.Events.Drivers
+{
+    public class EventPartDriver : ContentPartDriver<EventPart>
+    {
+        protected override string Prefix
+        {
+            get { return "EventPart"; }
+        }
+
+        protected override DriverResult Display(EventPart part, string displayType, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Event", () => shapeHelper.Parts_Event(EventPart: part));
+        }
+
+        protected override DriverResult Editor(EventPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Event_Edit", () => shapeHelper.EditorTemplate(TemplateName: "Parts/Event", Model: part, Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(EventPart part, Orchard.ContentManagement.IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Events/Handlers/EventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using RiceAlumni.Events.Models;
+
+namespace RiceAlumni.Event.Handlers
+{
+    public class EventHandler : ContentHandler
+    {
+        public EventHandler(IRepository<EventPartRecord> eventPartRepository)
+        {
+            Filters.Add(StorageFilter.For(eventPartRepository));
+        }
+    }
+}
