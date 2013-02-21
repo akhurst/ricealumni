5894bd04f17ebd334040cae37ebba316742aca9a
Allen Hurst <allenhurst@gmail.com>
2013-02-21 16:23:56 -0600

    fixing admin path link

---
+++ orchard/src/Orchard.Web/Modules/RiceAlumni.Core/Helpers/RiceUrlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard;
+using Orchard.Environment.Configuration;
+
+namespace RiceAlumni.Core.Helpers
+{
+    public static class RiceUrlHelperExtensions
+    {
+        public static string SiteRelativeUrl(this UrlHelper urlHelper, string path, WorkContext workContext)
+        {
+            var shellSettings = workContext.Resolve<ShellSettings>();
+
+            return urlHelper.Content(string.Format("~/{0}/{1}",shellSettings.RequestUrlPrefix,path.TrimStart('/','~')));
+        }
+    }
+}
